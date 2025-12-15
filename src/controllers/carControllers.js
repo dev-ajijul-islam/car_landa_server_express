effb3a364a1b5340aef5c18277fe31effb92346c
dev-ajijul-islam <devajijulislam@gmail.com>
2025-12-15 23:38:35 +0600

    getting car api with queries implemented

--- src/controllers/carControllers.js
+++ src/controllers/carControllers.js
@@ -1,10 +1,29 @@
 const Car = require("../models/Car");
 const connectDB = require("../config/db.js");
+const { Query } = require("firebase-admin/firestore");
 
 const getCars = async (req, res) => {
+  const { limit, carTypeId, title, isFeatured, isHotDeal, isActive } =
+    await req.query;
+  let query = {};
   try {
     await connectDB();
-    const result = await Car.find();
+    if (carTypeId) {
+      query.carTypeId = carTypeId;
+    }
+    if (title) {
+      query.title = { $regex: title, $options: "i" };
+    }
+    if (isFeatured) {
+      query["flags.isFeatured"] = isFeatured == "true";
+    }
+    if (isHotDeal) {
+      query["flags.isHotDeal"] = isHotDeal == "true";
+    }
+    if (isActive) {
+      query["flags.isActive"] = isActive == "true";
+    }
+    const result = await Car.find(query).limit(limit);
     res.status(200).send({
       success: true,
       message: "Car Loaded successfully",
