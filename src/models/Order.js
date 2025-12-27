5301178aaf858b488fa3462ade6c7290d8b88cd3
dev-ajijul-islam <devajijulislam@gmail.com>
2025-12-27 20:30:45 +0600

    order model created

---
+++ src/models/Order.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    carId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Car',
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', 
+        required: true
+    },
+    deliveryOption: {
+        type: String,
+        enum: ['Luanda', 'Doorstep'],
+        required: true
+    },
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    paymentMethod: {
+        type: String,
+        required: true
+    },
+    paymentStatus: {
+        type: String,
+        enum: ['Pending', 'Paid', 'Failed'],
+        default: 'Pending'
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Order', orderSchema);
