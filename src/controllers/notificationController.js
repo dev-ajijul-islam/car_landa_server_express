a17c6ae2f186faa82d6c0dcb90065d40ac51818d
dev-ajijul-islam <devajijulislam@gmail.com>
2026-01-17 17:27:31 +0600

    notification controller created

---
+++ src/controllers/notificationController.js
@@ -0,0 +1,38 @@
+const Notification = require("../models/Notification.js");
+const connectDB = require("../config/db");
+
+const createNotification = async (req, res) => {
+  const data = await req.body;
+  await connectDB();
+  try {
+    const result = await Notification.create(data);
+    res.status(200).send({
+      success: true,
+      message: "notification get succesfully",
+      body: result,
+    });
+  } catch (e) {
+    res.status(400).send({
+      success: true,
+      message: `notification create Failed ${e}`,
+    });
+  }
+};
+
+const getNotifications = async (req, res) => {
+  const { userId } = await req.params;
+  await connectDB();
+  try {
+    const result = await Notification.find({ userId });
+    res.status(200).send({
+      success: true,
+      message: "notification get succesfully",
+      body: result,
+    });
+  } catch (e) {
+    res.status(400).send({
+      success: true,
+      message: `notification get Failed ${e}`,
+    });
+  }
+};
