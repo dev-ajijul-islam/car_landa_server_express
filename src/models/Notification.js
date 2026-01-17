f250ca252e2bdbf72974dbef5ade150e81f56983
dev-ajijul-islam <devajijulislam@gmail.com>
2026-01-17 16:52:37 +0600

    notification model created

---
+++ src/models/Notification.js
@@ -0,0 +1,24 @@
+const  mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    body : {
+        type : String,
+        required : true
+    },
+    CreatedAt : {
+        type : Date,
+        default : Date.now()
+    },
+    userId : {
+        type : String,
+        required : true
+    }
+});
+
+const model  = mongoose.models.notification || mongoose.model("notification",notificationSchema);
+
+module.exports = model;
